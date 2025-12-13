3c232fe1d1bd70bed7a9237befc53e4ca86c3c01
Morten Elk <morten@elk.dk>
2025-12-13 13:23:59 +0100

    try to fix error on Netlify

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   serverExternalPackages: ["@mastra/*"],
+  // Ensure compatibility with Netlify
+  output: "standalone",
 };
 
 export default nextConfig;
